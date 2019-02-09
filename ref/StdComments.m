e72c206e6c92cfd549f4c7e579b15e0c07a3b6ff
Justin Reina <justinmreina@gmail.com>
2019-02-08 16:18:14 -0800

    (+)    add application main

---
+++ ref/StdComments.m
@@ -0,0 +1,76 @@
+%%-----------------------------------------------------------------------------------------------------------------------------------/
+ %  @file		x
+ %  @brief		x
+ % 	@details	x
+ %
+ % 	@author		Justin Reina, Firmware Engineer, Company Name
+ % 	@created	x
+ % 	@last rev	x
+ %
+ %
+ % 	@notes		x
+ %
+ % 	@section	Opens
+ %  	none current
+ % 
+ % 	@section	Legal Disclaimer
+ % 		© Year Company, All rights reserved. All contents of this source file and/or any other related source files 
+ %		are the explicit property of Company Name. Do not distribute. Do not copy. 
+ %
+%%-----------------------------------------------------------------------------------------------------------------------------------/
+clc, clear all, format compact                                              % Setup
+disp("Welcome to StdComments.m");
+
+
+%%-----------------------------------------------------------------------------------------------------------------------------------/
+ %  @fcn		someFunction
+ %  @brief		x
+ % 	@details	x
+ %
+ %  @param		[in]	name	descrip
+ %
+ %  @return		descrip
+ %
+ %  @pre        x
+ %
+ %  @post		x
+ %
+ %  @section	Operation
+ %		x
+ %		
+ %  @section	Opens
+ %  	x
+ %
+ %  @section	Hazards & Risks
+ %  	x
+ %
+ %  @section	Todo
+ %		x
+ %
+ %  @section	Timing
+ %  	x
+ %
+ %  @note		x
+ %
+%%-----------------------------------------------------------------------------------------------------------------------------------/
+function someRslt = someFunction( x ) 
+    someRslt = x;
+end % of function
+
+%%------------------------------------------------------------A Delimiting Note-----------------------------------------------------%%
+
+%%**********************************************************************************************************************************%%
+%%																SECTION HEADER														%%
+%%**********************************************************************************************************************************%%
+
+																			% note to the side
+
+% a note
+
+%% a note on top
+% that spans multiple lines
+
+%% PROTOTYPES, DECLARATIONS AND PREPROC
+
+%% [1/x]-------------------------------------------STEP 1---------------------------------------------------------------------------%%
+
---
+++ src/WBDUpdate.java
@@ -0,0 +1,38 @@
+/************************************************************************************************************************************/
+/** @file		WBDUpdate.java
+ * 	@brief		WorkByDay check and update for directories
+ * 	@details	x
+ *
+ * 	@author		Justin Reina, Firmware Engineer, Company Name
+ * 	@created	2/8/19
+ * 	@last rev	2/8/19
+ *
+ *
+ * 	@notes		x
+ *
+ * 	@section	Opens
+ * 			none current
+ *
+ * 	@section	Legal Disclaimer
+ * 			© Year Company Name, All rights reserved. All contents of this source file and/or any other related source  
+ *			files are the explicit property of Company Name. Do not distribute. Do not copy.
+ */
+/************************************************************************************************************************************/
+public class WBDUpdate {
+
+	/************************************************************************************************************************************/
+	/**	@fcn		public static void main(String[] args)
+	 *  @brief		Application entry point
+	 *  @details	x
+	 *
+	 *  @param		[in] (String[]) args - 
+	 */
+	/************************************************************************************************************************************/
+	public static void main(String[] args) {
+
+		System.out.println("WBDUpate complete.");
+		
+		return;
+	}
+
+}
