9fd894fc209f32933f4ad603ac8b3cb8f3562e77
Justin Reina <justinmreina@gmail.com>
2019-02-08 16:23:50 -0800

    (C)    add string print example

--- src/WBDUpdate.java
+++ src/WBDUpdate.java
@@ -20,6 +20,9 @@
 /************************************************************************************************************************************/
 public class WBDUpdate {
 
+	//Global Variables
+	public static String[] toppings = {"Cheese", "Pepperoni", "Black Olives"};
+	
 	/************************************************************************************************************************************/
 	/**	@fcn		public static void main(String[] args)
 	 *  @brief		Application entry point
@@ -30,6 +33,17 @@ public class WBDUpdate {
 	/************************************************************************************************************************************/
 	public static void main(String[] args) {
 
+		//Init
+		toppings[0] = "Justin";
+		toppings[1] = "likes";
+		toppings[2] = "cheese";
+		
+		// Store the formatted string in 'result'
+		String message = String.format("%s %s %s.", toppings[0], toppings[1], toppings[2]);
+
+		// Write the result to standard output
+		System.out.println(message);
+		
 		System.out.println("WBDUpate complete.");
 		
 		return;
